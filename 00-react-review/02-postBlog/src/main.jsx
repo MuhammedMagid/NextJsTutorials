45f64dc59f59edf8355fb842c4ad7db9201168fe
Muhammed Magid <muhammedmagid@gmail.com>
2024-12-03 23:54:52 +0200

    React router is added

--- 00-react-review/02-postBlog/src/main.jsx
+++ 00-react-review/02-postBlog/src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import CreatePost from "../components/createPost.jsx";
+import PostList from "../components/postList.jsx";
+
+import {RouterProvider , createBrowserRouter  } from "react-router-dom";
+
+
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/createpost",
+    element: <CreatePost />,
+  },
+  {
+    path: "/postslist",
+    element: <PostList />,
+  },
+
+  ]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
